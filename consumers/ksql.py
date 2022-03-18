6bfc6e87ec5130736c113eb5d5b09beb7b036a1c
Stefan Huber <stefan.huber3@baywa.de>
2022-03-18 22:42:54 +0100

    Add ksql tables

--- consumers/ksql.py
+++ consumers/ksql.py
@@ -12,25 +12,26 @@ logger = logging.getLogger(__name__)
 
 KSQL_URL = "http://localhost:8088"
 
-#
-# TODO: Complete the following KSQL statements.
-# TODO: For the first statement, create a `turnstile` table from your turnstile topic.
-#       Make sure to use 'avro' datatype!
-# TODO: For the second statment, create a `turnstile_summary` table by selecting from the
-#       `turnstile` table and grouping on station_id.
-#       Make sure to cast the COUNT of station id to `count`
-#       Make sure to set the value format to JSON
-
 KSQL_STATEMENT = """
+SET 'auto.offset.reset' = 'earliest';
+
 CREATE TABLE turnstile (
-    ???
+    station_id bigint,
+    station_name varchar,
+    line integer
 ) WITH (
-    ???
+    kafka_topic='cta.turnstiles',
+    key='station_id',
+    value_format='avro'
 );
 
-CREATE TABLE turnstile_summary
-WITH (???) AS
-    ???
+CREATE TABLE turnstile_summary 
+WITH (value_format='json')
+AS
+    SELECT station_id, count(*) as count
+    FROM turnstile
+    GROUP BY station_id 
+;
 """
 
 
--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -21,17 +21,16 @@ class Turnstile(Producer):
 
     def __init__(self, station):
         """Create the Turnstile"""
-        station_name = (
-            station.name.lower()
-            .replace("/", "_and_")
-            .replace(" ", "_")
-            .replace("-", "_")
-            .replace("'", "")
-        )
+        # station_name = (
+        #     station.name.lower()
+        #     .replace("/", "_and_")
+        #     .replace(" ", "_")
+        #     .replace("-", "_")
+        #     .replace("'", "")
+        # )
 
         super().__init__(
-            # TODO: Come up with a better topic name
-            f"cta.turnstiles.{station_name}",
+            f"cta.turnstiles",
             key_schema=Turnstile.key_schema,
             value_schema=Turnstile.value_schema,
             num_partitions=1,
